9f6871577619326c08e550303a48942353aabd0b
Ted Klingenberg <ted@doseofted.com>
2022-08-13 11:42:22 -0400

    Changed names of components in Solid example

--- events-solid/src/index.tsx
+++ events-solid/src/index.tsx
@@ -1,25 +1,24 @@
-/* @refresh reload */
 import { render } from 'solid-js/web';
 import { Component, JSX } from "solid-js"
 type HtmlProps = JSX.HTMLAttributes<HTMLDivElement>
 
 const useDivAsComponent = true
 
-const ChildComp: Component<HtmlProps> = (props) => {
-  const log = (name: string) => console.log("child", name)
+const ComponentInner: Component<HtmlProps> = (props) => {
+  const log = (name: string) => console.log("inner", name)
   const Div = (p: HtmlProps) => <div {...p} />
   return useDivAsComponent
     ? <Div {...props} onMouseEnter={[log, "enter"]} onMouseLeave={[log, "leave"]} />
     : <div {...props} onMouseEnter={[log, "enter"]} onMouseLeave={[log, "leave"]} />
 }
 
-const ParentComp: Component<HtmlProps> = (p) => {
-  const log = (name: string) => console.log("parent", name)
-  return <ChildComp {...p} onMouseEnter={[log, "enter"]} onMouseLeave={[log, "leave"]} />
+const ComponentOuter: Component<HtmlProps> = (p) => {
+  const log = (name: string) => console.log("outer", name)
+  return <ComponentInner {...p} onMouseEnter={[log, "enter"]} onMouseLeave={[log, "leave"]} />
 }
 
 const style: JSX.CSSProperties = { background: "lightgrey", width: "100%", height: "100vh" }
 render(
-  () => <ParentComp style={style} />,
+  () => <ComponentOuter style={style} />,
   document.getElementById('app') as HTMLElement
 )
